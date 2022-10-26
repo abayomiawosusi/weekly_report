9edecbe12cd9253f3c3022ddc1cf8629b9890c6d
abayomiawosusi <113623622+abayomiawosusi@users.noreply.github.com>
2022-10-26 16:35:23 -0500

    Delete doctype directory

--- doctype/weekly_sales_report/test_weekly_sales_report.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, abayomi.awosusi@sgatechsolutions.com and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestWeeklySalesReport(unittest.TestCase):
-	pass
